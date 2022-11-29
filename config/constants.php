1612d7e1be64ce7eee0e253ba49fedf1b272ca0e
Sami_B <Samisbdm@gmail.com>
2022-11-29 16:59:00 +0100

    Bootsrap de l'app

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * --------------------------------------------------------------
+ * Les constantes
+ * 
+ * Les constantes jouent le role de raccourci dans notre projet
+ * --------------------------------------------------------------
+ */
+
+/**
+ * Cette constante represente le chemin racine du dossier de l'app
+ */
+ const ROOT = __DIR__. "/../";
